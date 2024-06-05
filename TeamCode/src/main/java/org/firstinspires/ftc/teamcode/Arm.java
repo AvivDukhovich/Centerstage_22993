9fcea3676492f91c2674096f2a371d6b503c4dd7
Hava <avivaviv11223344@gmail.com>
2024-06-05 12:17:30 +0300

    fixed unloading arm values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -17,7 +17,7 @@ public class Arm {
     static private int hold_position2 = 0;
     static private int MAX_SPEED = 300;
     static private int HANGING_POSITION = -1195;
-    static public int UNLOADING_POSITION = -1625;
+    static public int UNLOADING_POSITION = -1605;
     static public boolean HANGING_MODE_ACTIVE = false;
     static public boolean DPAD_PRESSED = false;
     static public boolean LOADING_MODE_ACTIVE = false;
